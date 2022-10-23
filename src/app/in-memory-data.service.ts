3d1c114e843a3c3927ff880875876743eb71d897
fefeo <glinkowski@onet.eu>
2022-10-23 09:49:46 +0200

    Started changing to vocabulary app, added translations to words list

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,15 +8,15 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 12, name: 'Dr. Nice' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr. IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 12, name: 'Bike', translation: 'Rower' },
+      { id: 13, name: 'Wheel', translation: 'Koło' },
+      { id: 14, name: 'Can', translation: 'Puszka' },
+      { id: 15, name: 'Plate', translation: 'Talerz' },
+      { id: 16, name: 'Bowl', translation: 'Miska' },
+      { id: 17, name: 'Fork', translation: 'Widelec' },
+      { id: 18, name: 'Knife', translation: 'Nóż' },
+      { id: 19, name: 'Watch', translation: 'Zegarek' },
+      { id: 20, name: 'Screen', translation: 'Ekran' }
     ];
     return {heroes};
   }
